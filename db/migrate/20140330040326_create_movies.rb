c5a0b361822462fb5a4315ab6c07db46de45840a
Cody Palmer <cody@artemis.net>
2014-03-29 22:15:17 -0600

    Creating the movie model

---
+++ db/migrate/20140330040326_create_movies.rb
@@ -0,0 +1,11 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |t|
+      t.string  'description'
+      t.string  'title'
+      t.time    'length'
+
+      t.timestamps
+    end
+  end
+end
