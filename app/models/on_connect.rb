0dab15990083a7cee217ac2e606fde2f5d1479e5
Cody Palmer <cody@artemis.net>
2014-04-25 22:29:14 -0600

    First pass to make onconnect API work

--- app/models/on_connect.rb
+++ app/models/on_connect.rb
@@ -1,6 +1,12 @@
 class OnConnect
+  require 'net/http'
+
+  def initialize
+    @endpoint = "http://data.tmsapi.com/v1/movies/showings/"
+  end
 
   def find_by_zipcode(zip)
-    "You are looking in the zipcode: #{zip}"
+    output = Net::HTTP.get(@endpoint + "?zip=#{zip}&api_key=zwhkp5tk7wenzuvmnavq5f8k")
+    output
   end
 end
