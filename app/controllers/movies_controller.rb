8e5090ed8b276a95baa208ff12d3d2e1c1b98eee
Cody Palmer <cody@artemis.net>
2014-08-23 15:26:42 -0600

    Adding uniq on movie.theatres
    * movie.theatres returns duplicated theatres when more than one showtime
    * is in a theatre.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,11 +2,11 @@ require './spec/support/webmock_onconnect_responses.rb'
 
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
-    # @movie_service = MovieService.new
+    @movie_service = MovieService.new
     # output = @movie_service.find_by_zipcode(80222)
     # @movie_service.parse_zipcode_payload(output)
-    # @movie_service.parse_zipcode_payload(WebmockOnconnectResponse.zipcode_response(80222))
+    @movie_service.parse_zipcode_payload(WebmockOnconnectResponse.zipcode_response(80222))
+    @movies = Movie.all
   end
 
   def show
