f885b843f59573912794e1c64bc9fc5d6bb09dfc
Cody Palmer <cody@artemis.net>
2014-08-23 16:35:13 -0600

    Create theatre#index

--- app/controllers/theatres_controller.rb
+++ app/controllers/theatres_controller.rb
@@ -2,10 +2,12 @@ require './spec/support/webmock_onconnect_responses.rb'
 
 class TheatresController < ApplicationController
   def index
-    @theatres = Theatres.al
+    @movie_service = MovieService.new
+    @movie_service.parse_zipcode_payload(WebmockOnconnectResponse.zipcode_response(80222))
+    @theatres = Theatre.all
   end
 
   def show
-    @theatre = Theatre.where(id: params[id])
+    @theatre = Theatre.find(params[:id])
   end
 end
