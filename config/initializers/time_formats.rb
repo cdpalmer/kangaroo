10b42baa8d0d47af7627938b59fea3f3cd92937b
Cody Palmer <cody@artemis.net>
2014-09-09 19:50:55 -0600

    Updating showtime index to readible start AND endtimes

---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,15 @@
+Time::DATE_FORMATS[:long_with_zone] = '%b %d, %Y %H:%M %Z'
+Time::DATE_FORMATS[:us_date] = '%m-%d-%y'
+Time::DATE_FORMATS[:us_date_slashes] = '%m/%d/%y'
+Time::DATE_FORMATS[:short_date] = '%m%d%y'
+Time::DATE_FORMATS[:time_with_zone] = '%H:%M %Z'
+
+Time::DATE_FORMATS[:date_time_and_zone] = '%m/%d/%Y %H:%M %Z'
+Time::DATE_FORMATS[:date_with_time] = '%H:%M %b %d %Y'
+Time::DATE_FORMATS[:date] = '%b %d %Y'
+
+Date::DATE_FORMATS[:long_with_zone] = '%b %d, %Y %H:%M %Z'
+Date::DATE_FORMATS[:us_date] = '%m-%d-%y'
+Date::DATE_FORMATS[:us_date_slashes] = '%m/%d/%y'
+Date::DATE_FORMATS[:short_date] = '%m%d%y'
+Date::DATE_FORMATS[:time_with_zone] = '%H:%M %Z'
