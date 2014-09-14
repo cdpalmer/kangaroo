65f7b0a6066441a1ab82f11e3210e5fa35eab61d
Cody Palmer <cody@artemis.net>
2014-09-14 17:07:11 -0600

    Updated marathon spec.  Onward to implementation!

--- spec/models/theatre_spec.rb
+++ spec/models/theatre_spec.rb
@@ -9,31 +9,36 @@ describe Theatre do
     let(:movie_4) { FactoryGirl.create(:movie, duration: 90) }
     let(:showtime_1) {
       FactoryGirl.create(:showtime,
-                         start_time: Time.now.to_i,
+                         start_time: Time.new(2014, 9, 9, 13, 0).to_i,
                          theatre_id: theatre.id,
                          movie_id: movie_1.id)
     }
     let(:showtime_2) {
       FactoryGirl.create(:showtime,
-                         start_time: Time.now.to_i,
+                         start_time: Time.new(2014, 9, 9, 11, 30).to_i,
                          theatre_id: theatre.id,
                          movie_id: movie_2.id)
     }
     let(:showtime_3) {
       FactoryGirl.create(:showtime,
-                         start_time: Time.now.to_i,
+                         start_time: Time.new(2014, 9, 9, 10, 0).to_i,
                          theatre_id: theatre.id,
                          movie_id: movie_3.id)
     }
     let(:showtime_4) {
       FactoryGirl.create(:showtime,
-                         start_time: Time.now.to_i,
+                         start_time: Time.new(2014, 9, 9, 8, 0).to_i,
                          theatre_id: theatre.id,
                          movie_id: movie_4.id)
     }
 
     it 'returns all marathons' do
-      expect( theatre.marathons ).to eq( [showtime_3, showtime_2, showtime_1] )
+      output = theatre.marathons
+      expected = [[showtime_3, showtime_2, showtime_1],
+                  [showtime_2, showtime_1],
+                  [showtime_3, showtime_2]]
+      # expect the array intersection to equal all items in expected
+      expect( (output & expected ).count ).to eq( expected.count )
     end
   end
 end
