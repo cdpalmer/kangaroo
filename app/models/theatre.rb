ffd7f04c9ada6b606999c93a767494ebea578e4e
Cody Palmer <cody.palmer08@gmail.com>
2015-02-26 23:44:33 -0700

    For lands sake! Got marathons working!

--- app/models/theatre.rb
+++ app/models/theatre.rb
@@ -3,6 +3,33 @@ class Theatre < ActiveRecord::Base
   has_many :showtimes
 
   def marathons
-    [Showtime.first, Showtime.last]
+    @hop_window = 15
+    @showtimes = showtimes.sort { |a,b| a.start_time <=> b.start_time }
+    @marathons = []
+    @showtimes.each_with_index do |showtime, index|
+      marathon = [showtime]
+
+      if showtime != @showtimes.last
+        next_showtime = find_next_showtime(@showtimes.from(@showtimes.index(showtime)), showtime)
+        while next_showtime
+          marathon << next_showtime
+          @marathons << marathon.dup
+          next_showtime = find_next_showtime(@showtimes.from(@showtimes.index(next_showtime)), next_showtime)
+        end
+      end
+    end
+
+    @marathons
+  end
+
+  def find_next_showtime(showtimes, showtime)
+    current_endtime = showtime.start_time + (showtime.movie.duration * 60)
+    showtimes.each do |st|
+      if st.start_time.between?(current_endtime - @hop_window, current_endtime + @hop_window)
+        return st
+      end
+    end
+
+    return nil
   end
 end
--- spec/models/theatre_spec.rb
+++ spec/models/theatre_spec.rb
@@ -2,30 +2,30 @@ require 'spec_helper'
 
 describe Theatre do
   describe '#marathons' do
-    let(:theatre) { FactoryGirl.create(:theatre) }
-    let(:movie_1) { FactoryGirl.create(:movie, duration: 90) }
-    let(:movie_2) { FactoryGirl.create(:movie, duration: 90) }
-    let(:movie_3) { FactoryGirl.create(:movie, duration: 90) }
-    let(:movie_4) { FactoryGirl.create(:movie, duration: 90) }
-    let(:showtime_1) {
+    let!(:theatre) { FactoryGirl.create(:theatre) }
+    let!(:movie_1) { FactoryGirl.create(:movie, duration: 90) }
+    let!(:movie_2) { FactoryGirl.create(:movie, duration: 90) }
+    let!(:movie_3) { FactoryGirl.create(:movie, duration: 90) }
+    let!(:movie_4) { FactoryGirl.create(:movie, duration: 90) }
+    let!(:showtime_1) {
       FactoryGirl.create(:showtime,
                          start_time: Time.new(2014, 9, 9, 13, 0).to_i,
                          theatre_id: theatre.id,
                          movie_id: movie_1.id)
     }
-    let(:showtime_2) {
+    let!(:showtime_2) {
       FactoryGirl.create(:showtime,
                          start_time: Time.new(2014, 9, 9, 11, 30).to_i,
                          theatre_id: theatre.id,
                          movie_id: movie_2.id)
     }
-    let(:showtime_3) {
+    let!(:showtime_3) {
       FactoryGirl.create(:showtime,
                          start_time: Time.new(2014, 9, 9, 10, 0).to_i,
                          theatre_id: theatre.id,
                          movie_id: movie_3.id)
     }
-    let(:showtime_4) {
+    let!(:showtime_4) {
       FactoryGirl.create(:showtime,
                          start_time: Time.new(2014, 9, 9, 8, 0).to_i,
                          theatre_id: theatre.id,
@@ -37,8 +37,9 @@ describe Theatre do
       expected = [[showtime_3, showtime_2, showtime_1],
                   [showtime_2, showtime_1],
                   [showtime_3, showtime_2]]
+
       # expect the array intersection to equal all items in expected
-      expect( (output & expected ).count ).to eq( expected.count )
+      expect( (output & expected).count ).to eq( expected.count )
     end
   end
 end
