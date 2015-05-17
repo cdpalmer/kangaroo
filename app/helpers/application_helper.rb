456304377699a262886117460eaf861d3b84bfab
Cody Palmer <cody.palmer08@gmail.com>
2015-05-16 19:29:33 -0600

    Style cleanup for query.  Movie#index table cleanup.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def determine_end_time(start, length)
     start + length*60
   end
+
+  def duration_print(minutes)
+    "#{minutes/60} hour(s) #{minutes%60} minutes"
+  end
 end
