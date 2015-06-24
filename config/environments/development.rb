fee5a471db5a3fa1f31b3eca979f257151afdbe7
Cody Palmer <cody.palmer08@gmail.com>
2015-06-23 18:59:55 -0600

    config changes to production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,5 +25,6 @@ Kangaroo::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
+  config.serve_static_assets = true
   config.assets.debug = true
 end
