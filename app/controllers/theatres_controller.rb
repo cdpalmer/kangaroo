1539f33668b37fef991bb40c2687cf00e924f7b8
Cody Palmer <cody.palmer08@gmail.com>
2015-10-13 21:30:57 -0600

    adding json resource to theatres

--- app/controllers/theatres_controller.rb
+++ app/controllers/theatres_controller.rb
@@ -5,6 +5,11 @@ class TheatresController < ApplicationController
     if params[:zip_code]
       @reference = params[:zip_code]
       @theatres = Theatre.select { |t| t.zipcodes.map(&:value).include?(params[:zip_code]) }
+
+      respond_to do |f|
+        f.json { render json: @theatres }
+        f.html { render }
+      end
     else
       @reference = 'all'
       @theatres = Theatre.all
@@ -15,5 +20,11 @@ class TheatresController < ApplicationController
     @theatre = Theatre.find(params[:id])
     @movies = @theatre.movies.uniq
     @marathons = @theatre.marathons
+
+    respond_to do |f|
+      # make a decorator or serializer for json. Needs movies and marathons
+      f.json { render json: @theatre }
+      f.html { render }
+    end
   end
 end
